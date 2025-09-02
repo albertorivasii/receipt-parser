cc0ccbe16b109175db147856665794c9e0bd5db1
Alberto Rivas II <albertorivas16@gmail.com>
2025-09-01 18:28:45 -0700

    Minor edits and comments

--- services/poll-textract.ts
+++ services/poll-textract.ts
@@ -1,7 +1,6 @@
 import { type TextractClient } from "@aws-sdk/client-textract";
 
 
-
 // poller output
 type TextractPollOk = { ok: true; raw:unknown };
 type TextractPollErr = { ok: false; error:string; status?:number, requestId?:string };
@@ -11,11 +10,15 @@ type ReceiptDoc = {
     pass:string;
 }
 
-type ReceiptParseOk = { ok: true; data: ReceiptDoc };
+// receipt parse outputs
+type ReceiptParseOk = { ok: true; raw: ReceiptDoc };
 type ReceiptParseErr = { ok: false, error: string; };
 type ReceiptParseResult = ReceiptParseOk | ReceiptParseErr
 
 
-export const getReceiptAnalysis = (client:TextractClient, jobId:string) => {
-
+export const getReceiptAnalysis = (client:TextractClient, jobId:string): TextractPollResult => {
+    const output = {ok: true, raw:"abc" }
+    
+    return output 
 }
+
